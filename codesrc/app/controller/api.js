2db8b53c2875aaf406e39cc2527063dca05db96c
luogf <519665744@qq.com>
2019-08-29 17:42:54 +0800

    开始构建封面页

--- codesrc/app/controller/api.js
+++ codesrc/app/controller/api.js
@@ -7,42 +7,59 @@ class Main {
     static async getMenus(ctx, next) {
         try {
             const { fid } = ctx.params;
-            let body = await request.get(util.createTopUrl(config.domain.uri, config.domain.fid));
-            body = JSON.parse(body);
-            if (body.Variables.sublist.length) {    ///如果有子菜单.当前fid是获取公司名目
-                let sublist = body.Variables.sublist;
-                sublist = [{fid: config.domain.fid, name: '全部'}].concat(sublist);
+            if (fid != '0') {
+                let body = await request.get(util.createTopUrl(config.domain.uri, config.domain.fid));
+                body = JSON.parse(body);
+                if (body.Variables.sublist.length) {    ///如果有子菜单.当前fid是获取公司名目
+                    let sublist = body.Variables.sublist;
+                    sublist = [{ fid: config.domain.fid, name: '全部' }].concat(sublist);
 
-                let articles = [];
-                if (fid == config.domain.fid) { //如果是all
-                    for (let i = 0; i < sublist.length; i++) {
-                        let subBody = await request.get(util.createTopUrl(config.domain.uri, sublist[i].fid));
+                    let articles = [];
+                    if (fid == config.domain.fid) { //如果是all
+                        for (let i = 0; i < sublist.length; i++) {
+                            let subBody = await request.get(util.createTopUrl(config.domain.uri, sublist[i].fid));
+                            subBody = JSON.parse(subBody);
+                            articles = articles.concat(subBody.Variables.forum_threadlist);
+                        }
+                    } else {
+                        let subBody = await request.get(util.createTopUrl(config.domain.uri, fid));
                         subBody = JSON.parse(subBody);
                         articles = articles.concat(subBody.Variables.forum_threadlist);
                     }
-                } else {
-                    let subBody = await request.get(util.createTopUrl(config.domain.uri, fid));
-                    subBody = JSON.parse(subBody);
-                    articles = articles.concat(subBody.Variables.forum_threadlist);
-                }
 
-                let renderData =  {
-                    title: body.Variables.forum.name,
-                    menus: sublist, 
-                    allArticles: articles,
-                    fid : fid
-                }
+                    let renderData = {
+                        title: body.Variables.forum.name,
+                        menus: sublist,
+                        allArticles: articles,
+                        fid: fid
+                    }
 
-                if (fid == config.domain.fid) {
-                    renderData.allShow = 'allShow';
-                }
+                    if (fid == config.domain.fid) {
+                        renderData.allShow = 'allShow';
+                        if (body.Variables.forum_threadlist.length) {
+                            sublist.push({
+                                fid: '0',
+                                name: body.Variables.forum_threadlist[0].subject
+                            })
+                        }
+                    }
 
-                await ctx.render('home', renderData);
+                    await ctx.render('home', renderData);
+                }
+            } else {
+
             }
-            
             ///ctx.body = 'Hello';
         } catch (e) {
             logger.error(e.message);
+        }
+    }
+
+    static async loadArticle(ctx, next) {
+        try {
+            let body = await request.get('https://yuxi.shaobaogu.com.cn/api/mobile/index.php?version=4&module=viewthread&tid=3');
+        } catch (e) {
+
         }
     }
 }
